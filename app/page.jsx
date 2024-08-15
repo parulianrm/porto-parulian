1d15f991dd868ac43ba00a9b1e03967fc386fc73
parulianrm <parulianrmanik@gmail.com>
2024-08-15 11:04:29 +0700

    make ui for homepage & add button socials media

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
+import Social from "@/components/Social";
 
 const Home = () => {
     return (
@@ -8,11 +9,34 @@ const Home = () => {
                 <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
                     {/* Text */}
                     <div className="">
-                        <span>Web Developer</span>
-                        <h1>
+                        <span className="text-xl">Web Developer</span>
+                        <h1 className="h1">
                             Hello I'm <br />
-                            <span>Parulian R Manik</span>
+                            <span className="text-accent">
+                                Parulian R Manik
+                            </span>
                         </h1>
+                        <p className="max-w-[500px] mb-9 text-white/80">
+                            I have a strong passion for technology, particularly
+                            in web development.
+                        </p>
+                        {/* button and socials */}
+                        <div className="flex flex-col xl:flex-row items-center gap-8">
+                            <Button
+                                variant="outline"
+                                size="lg"
+                                className="uppercase flex items-center gap-2"
+                            >
+                                <span>Download CV </span>
+                                <FiDownload className="text-xl" />
+                            </Button>
+                            <div className="mb-8 xl:mb-0">
+                                <Social
+                                    containerStyles="flex gap-6"
+                                    iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                                />
+                            </div>
+                        </div>
                     </div>
                     {/* Photo */}
                     <div>photo</div>
---
+++ components/Social.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn, FaInstagram, FaTwitter } from "react-icons/fa";
+
+const socials = [
+    { icon: <FaGithub />, path: "https://github.com/parulianrm" },
+    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/parulianrm/" },
+    { icon: <FaInstagram />, path: "https://www.instagram.com/lianrosado_/" },
+    { icon: <FaTwitter />, path: "https://x.com/LianRosado_" },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+    return (
+        <div className={containerStyles}>
+            {socials.map((item, index) => {
+                return (
+                    <Link key={index} href={item.path} className={iconStyles}>
+                        {item.icon}
+                    </Link>
+                );
+            })}
+        </div>
+    );
+};
+
+export default Social;
