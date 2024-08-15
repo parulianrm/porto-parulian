d02d800bd6437d94cd16b2d9c87a8f6747847067
parulianrm <parulianrmanik@gmail.com>
2024-08-15 10:04:38 +0700

    home page build

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <main>homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+    return (
+        <section className="h-full">
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+                    {/* Text */}
+                    <div className="">
+                        <span>Web Developer</span>
+                        <h1>
+                            Hello I'm <br />
+                            <span>Parulian R Manik</span>
+                        </h1>
+                    </div>
+                    {/* Photo */}
+                    <div>photo</div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
