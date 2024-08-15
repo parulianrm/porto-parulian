9532211c5ac266e0810a7e371857f4af39cfefff
parulianrm <parulianrmanik@gmail.com>
2024-08-15 15:26:59 +0700

    service page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,87 @@
-const Services = () => {
-  return <div> services page</div>;  
-}
+"use client";
 
-export default Services
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const services = [
+    {
+        num: "01",
+        title: "Web Development",
+        descriptions:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et assumenda accusantium quisquam tempore sint fugiat illum? Totam.",
+        href: "",
+    },
+    {
+        num: "02",
+        title: "UI/UX Designer",
+        descriptions:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et assumenda accusantium quisquam tempore sint fugiat illum? Totam.",
+        href: "",
+    },
+    {
+        num: "03",
+        title: "Logo Designer",
+        descriptions:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et assumenda accusantium quisquam tempore sint fugiat illum? Totam.",
+        href: "",
+    },
+    {
+        num: "04",
+        title: "Graphic Designer",
+        descriptions:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et assumenda accusantium quisquam tempore sint fugiat illum? Totam.",
+        href: "",
+    },
+];
+
+const Services = () => {
+    return (
+        <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+            <div className="container mx-auto">
+                <motion.div
+                    initial={{ opcacity: 0 }}
+                    animate={{
+                        opacity: 1,
+                        transition: {
+                            delay: 2.4,
+                            duration: 0.4,
+                            ease: "easeIn",
+                        },
+                    }}
+                    className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+                >
+                    {services.map((services, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className="flex-1 flex flex-col justify-center gap-6 group"
+                            >
+                                <div className="w-full flex justify-between items-center">
+                                    <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration=500">
+                                        {services.num}
+                                    </div>
+                                    <Link
+                                        href={services.href}
+                                        className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                                    >
+                                        <BsArrowDownRight className="text-primary text-3xl" />
+                                    </Link>
+                                </div>
+                                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                                    {services.title}
+                                </h2>
+                                <p className="text-white/60">
+                                    {services.descriptions}
+                                </p>
+                                <div className="border-b border-accent/20 w-full"></div>
+                            </div>
+                        );
+                    })}
+                </motion.div>
+            </div>
+        </section>
+    );
+};
+
+export default Services;
