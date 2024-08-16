df989013cc9537f4cf2ffa33a5b0f0defbd81ed2
parulianrm <parulianrmanik@gmail.com>
2024-08-16 07:51:40 +0700

    save

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -10,7 +10,41 @@ import {
 } from "react-icons/fa";
 
 import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
-import {} from "react-icons/";
+
+const about = {
+  title: "About Me",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat totam quaerat iste sint reprehenderit porro.",
+  info:[
+    {
+      fieldName: "Name",
+      fieldValue: "Parulian Ramot Manik"
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "+6289-6734-19632"
+    },
+    {
+      fieldName: "Work Experience"
+      fieldValue: "6+ Years"
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Indonesia"
+    },
+    {
+      fieldName: "E-Mail",
+      fieldValue: "parulianrmanik@gmail.com"
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available"
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "Indonesia, English"
+    },
+  ],
+};
 
 const page = () => {
     return <div>Resume</div>;
