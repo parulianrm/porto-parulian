f9856060d1720b2ca181f97bbb7ca88cc319faa8
parulianrm <parulianrmanik@gmail.com>
2024-08-16 07:41:41 +0700

    try git ke master

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,8 +1,19 @@
 "use client";
 import Link from "next/link";
+import {
+    FaHtml5,
+    FaCss3,
+    FaJs,
+    FaReact,
+    FaFigma,
+    FaNodeJs,
+} from "react-icons/fa";
+
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import {} from "react-icons/";
 
 const page = () => {
-    return <div>page</div>;
+    return <div>Resume</div>;
 };
 
 export default page;
